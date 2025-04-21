098e4c62481179c4bc648cca053b46498e111d06
jd1t25 <mayureshsaitwal25@gmail.com>
2025-04-21 22:23:36 +0530

    Audio Script Sink Change

--- bin/audio_switcher.sh
+++ bin/audio_switcher.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-CURRENT_SINK=$(pacmd stat | grep "Default sink name" | sed 's/.*: *//')
-# speaker=
-# headphone=
-
-#echo $CURRENT_SINK
+CURRENT_SINK=$(pactl get-default-sink)
 
 	if [ "$CURRENT_SINK" == "alsa_output.usb-Conexant_CONEXANT_USB_AUDIO_000000000000-00.analog-stereo" ]
 	then
-		pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
+    pactl set-default-sink alsa_output.pci-0000_09_00.4.analog-stereo
 		notify-send -t 2500 "Audio 2 Speaker" --icon="~/.icons/speaker.png"
 		source ~/bin/set_border
-		# echo $speaker 
-		# bspc config focused_border_color '#80c484'
 	else
 		pactl set-default-sink alsa_output.usb-Conexant_CONEXANT_USB_AUDIO_000000000000-00.analog-stereo
 		notify-send -t 2500 "Audio 2 Headphones" --icon="~/.icons/headphones-solid.png"
 		source ~/bin/set_border
-		# echo $headphone
-		# bspc config focused_border_color '#8284cf'
 	fi
 
-# pkill -RTMIN+8 waybar
-# if pgrep -f "python3 bin/pulse-volume-watcher.py" > /dev/null
-# then
-# 		pkill -f "python3 bin/pulse-volume-watcher.py"
-# fi
-
-# nohup bash -c 'bin/pulse-volume-watcher.py | xob' &> /dev/null &
