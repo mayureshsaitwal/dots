736decabe98200f427abc21d36aea7d0977c4a65
jd1t25 <mayureshsaitwal25@gmail.com>
2025-12-23 14:01:47 +0530

    :tada: Add Oxocarbon nvim theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd[[colorscheme tokyonight-night]]
-  end
+	-- "folke/tokyonight.nvim",
+	"nyoom-engineering/oxocarbon.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		-- vim.cmd([[colorscheme tokyonight-night]])
+		vim.cmd([[colorscheme oxocarbon]])
+		vim.opt.background = "dark"
+	end,
 }
-
