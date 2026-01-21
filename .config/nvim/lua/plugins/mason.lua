1df0f94c8fe7370eb6586424453f52c73e238fa0
jd1t25 <mayureshsaitwal25@gmail.com>
2026-01-21 14:45:37 +0530

    :gear: add auto install mason lsp servers

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,23 @@
 return {
 	{
-		"mason-org/mason.nvim",
-		opts = {},
-	}
+		"mason-org/mason-lspconfig.nvim",
+		opts = { automatic_enable = true },
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				"basedpyright",
+				"ts_ls",
+				"rust-analyzer",
+				"stylua",
+				"lua-language-server",
+				"vim-language-server",
+			},
+		},
+	},
 }
-
